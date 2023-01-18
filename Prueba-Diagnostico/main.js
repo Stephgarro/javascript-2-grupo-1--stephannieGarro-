a42ba43c5f0901e6c76c9be3377be36f2d208ec7
stephgarro2001@gmail.com <109922003+Stephgarro@users.noreply.github.com>
2023-01-17 21:08:08 -0600

    i completed my first homework

---
+++ Prueba-Diagnostico/main.js
@@ -0,0 +1,124 @@
+// Prueba Diagnostico
+
+// I Parte: Teoría
+
+// 1. Respuesta pregunta 1
+
+// Colocar la respuesta acá
+
+//bueno los datos primitivos son los que no son metodos  los cuales son 
+ //string que basicamente se utiliza para ngresar valores d texto.
+ //number que basicamnete se utiliza para ingresar valores de numeros.
+ //boolen que determinan si un valor es verdadero  falso
+ //undifined se muestra cuado hay un valor indefinido 
+ //null determina i un valor es como inexistente por decirlo asi 
+
+// ====================================================================================================
+
+
+// 2. Respuesta pregunta 2
+
+// Colocar la respuesta acá
+
+//Las propiedades se pueden entender como un tipo de caracteristicas que en este caso se le dan a los objetos
+//en javascrip se conocen como variables las cuales se utlizan como para asignar un valor 
+// un ejemplo podria ser  
+// let mySelf{
+ /* name: 'stephannie',
+  appeldo: 'garro',
+  age: 21
+}*/
+
+// ====================================================================================================
+
+
+// 3. Respuesta pregunta 3
+
+// Colocar la respuesta acá
+/*se podria decir que la funcion del += es como sumarle valores de las vvariables en el caso de este ccodigo 
+lo que hace es sumar el valor se la dercha al de la derecha  por lo cual el console.log(list) daria el resultado = 4465
+por otro lado el console.log('texto de prueba') daria el resulatdo = ' 4465 texto de prueba'
+
+
+// ====================================================================================================
+
+
+// 4. Respuesta pregunta 4
+
+// Colocar la respuesta acá
+/* != se utiliza para comprobar si un valor es de igual tipo , por otro lado
+!== se utiliza para comprobar si son el mismo tipo pero no igual valor , o probar si son de doferente tipo
+ en  este ejemplo 
+
+const flag = false;
+flag != true; aqui quiere comprobar si es booleano 
+flag !== true; aqui si es boleano y el mismo valor osea flag
+
+en este ejemplo
+
+const num = 10;
+num != '10'; aqui esta probando si es deel mismo tipo
+num !== '10'; aqui si son el mismo tipo y vaalor que seria false ya que el la variable no  esta definida como string*/
+
+// ====================================================================================================
+
+
+// 5. Respuesta pregunta 5
+
+// Colocar la respuesta acá
+/*
+Se puede definir basicamente como las funciones a las cuales no se les asigna un nombre por o general se 
+utiliza la palbra function para determnar que es una functon y el lenguaje lo pueda comprender.
+*/
+
+// ====================================================================================================
+
+
+// 6. Respuesta pregunta 6
+
+// Colocar la respuesta acá
+
+/*
+Se podria decir que su diferencia esta en que las function declaration son las hacen una funcion de forma global
+ y las  function expression hacen funciones de un fragmento de codigo mas especifico 
+*/
+
+// ====================================================================================================
+
+
+// 7. Respuesta pregunta 7
+
+// Colocar la respuesta acá
+/*
+se puede decir que los metodos son propiedades que cumplen una funcionalidad especifica pero depeden principalmene
+de un objeto
+*/
+
+// ====================================================================================================
+
+
+// 8. Respuesta pregunta 8
+
+// Colocar la respuesta acá
+/*
+se podria decir qu recursividad es la accion que cumple una funcion al llamrse a ella misma para resolver
+subproblemas por decirlo de esta forma 
+*/
+
+// ====================================================================================================
+
+
+// II Parte: Ejercicios
+
+// 1. Respuesta pregunta 1
+
+// Colocar el código acá
+
+// ====================================================================================================
+
+
+// 2. Respuesta pregunta 2
+
+// Colocar el código acá
+
+// ====================================================================================================
